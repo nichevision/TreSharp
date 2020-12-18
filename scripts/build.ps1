3152731c21f7783d0ff91ef2a2398438715a30da
tfaris <ta.faris@gmail.com>
2020-12-18 14:49:36 -0500

    Use dotnet to build projects.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,19 +1,7 @@
-$VSWhere = Get-Command vswhere.exe -ErrorAction Ignore
-if (!$VSWhere)
-{
-    $VSWhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-    if (!(Test-Path $VSWhere))
-    {
-        throw "vswhere.exe command is required in order to find latest MSBuild. You must install Visual Studio 2017 or newer or add the command to your PATH."
-    }
-}
+Get-Command dotnet -ErrorAction Stop
 
-$MsBuildCommand = &$VSWhere -latest -requires Microsoft.Component.MSBuild -find "MSBuild\**\Bin\MSBuild.exe"
-if (!$MsBuildCommand) {
-    throw "msbuild is required."
-}
+dotnet build .\TreSharp\TreSharp.csproj -c Release /p:Platform=x86
+dotnet build .\TreSharp\TreSharp.csproj -c Release /p:Platform=x64
+dotnet build .\TreSharp\TreSharp.csproj -c Release /p:Platform=AnyCPU
 
-&$MsBuildCommand TreSharp.sln /p:Configuration=Release /t:Rebuild /p:Platform=x86 /verbosity:quiet
-&$MsBuildCommand TreSharp.sln /p:Configuration=Release /t:Rebuild /p:Platform=x64 /verbosity:quiet
-
-nuget pack ./nuget/TreSharp.nuspec /version $(git describe --tags)
+nuget pack ./nuget/TreSharp.nuspec -Version $(git describe --tags)
